f5911559e572a17118c9f2b3ad542b87218176cb
spawn150 <spawn150@gmail.com>
2017-03-09 17:51:18 +0100

    aggiungo ciclo di vita per esempio

--- app/src/main/java/it/englab/androidcourse/intentapplication/MainActivity.java
+++ app/src/main/java/it/englab/androidcourse/intentapplication/MainActivity.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
     private static final int REQUEST_CODE_DETAIL = 1;
 
     @Override
@@ -53,7 +55,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        Log.d(TAG, "onActivityResult");
 
         if (requestCode == REQUEST_CODE_DETAIL) {
             if (resultCode == RESULT_OK) {
